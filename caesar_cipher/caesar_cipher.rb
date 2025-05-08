08e676d73c893ce58445ca5575c3363767b3deea
Ryan Boudwin <rboudwin@student.hive.fi>
2025-05-08 13:23:43 +0300

    Handles spaces and punctuation appropriately now

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -6,8 +6,8 @@ def caesar_cipher(string, shift_factor)
       if (byte > 90 && byte <= 90 + shift_factor) ||(byte > 122 && byte <= 122 + shift_factor)
         byte -= 26
       end
-      result += byte.chr
     end
+    result += byte.chr
     }
   puts result
 end
