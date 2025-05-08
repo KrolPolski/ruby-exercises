aa5e3df983801257b06751f26f4bfafb79738740
Ryan Boudwin <rboudwin@student.hive.fi>
2025-05-08 11:25:19 +0300

    caesar cipher complete

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,13 +1,13 @@
 def caesar_cipher(string, shift_factor)
-  print "before: #{string}\n"
   result = ""
   string.each_byte {|byte|
-    if ((byte >= 65 && byte <= 90 - shift_factor) || (byte >= 97 && byte <= 122 - shift_factor))   
+    if ((byte >= 65 && byte <= 90) || (byte >= 97 && byte <= 122))   
       byte += shift_factor
+      if (byte > 90 && byte <= 90 + shift_factor) ||(byte > 122 && byte <= 122 + shift_factor)
+        byte -= 26
+      end
       result += byte.chr
     end
     }
-  print "after: #{result}"
+  puts result
 end
-
-caesar_cipher("abcde", 4)
