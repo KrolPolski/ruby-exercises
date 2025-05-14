c434359e0f1c47f6dd18324d2a988619a33b7583
Ryan Boudwin <ryanboudwin@outlook.com>
2025-05-14 23:04:07 +0300

    added rubocop to the caesar cipher

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,13 +1,15 @@
 def caesar_cipher(string, shift_factor)
-  result = ""
-  string.each_byte {|byte|
-    if ((byte >= 65 && byte <= 90) || (byte >= 97 && byte <= 122))   
-      byte += shift_factor
-      if (byte > 90 && byte <= 90 + shift_factor) ||(byte > 122 && byte <= 122 + shift_factor)
-        byte -= 26
+  result = ''
+  string.each_byte do |byte|
+      if ((byte >= 65 && byte <= 90) || (byte >= 97 && byte <= 122))   
+        byte += shift_factor
+        if (byte > 90 && byte <= 90 + shift_factor) ||(byte > 122 && byte <= 122 + shift_factor)
+          byte -= 26
+        end
       end
-    end
     result += byte.chr
-    }
+    end
   puts result
 end
+
+caesar_cipher("Hello dude", 4)
