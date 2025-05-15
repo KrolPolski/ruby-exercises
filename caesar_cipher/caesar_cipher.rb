0d0b4a90181850b4df7e5b43f335254d35e7b80c
Ryan Boudwin <rboudwin@student.hive.fi>
2025-05-15 10:08:25 +0300

    caesar_cipher corrected with rubocop

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,15 +1,13 @@
+# frozen_string_literal: false
+
 def caesar_cipher(string, shift_factor)
   result = ''
   string.each_byte do |byte|
-      if ((byte >= 65 && byte <= 90) || (byte >= 97 && byte <= 122))   
-        byte += shift_factor
-        if (byte > 90 && byte <= 90 + shift_factor) ||(byte > 122 && byte <= 122 + shift_factor)
-          byte -= 26
-        end
-      end
-    result += byte.chr
+    if (byte >= 65 && byte <= 90) || (byte >= 97 && byte <= 122)
+      byte += shift_factor
+      byte -= 26 if (byte > 90 && byte <= 90 + shift_factor) || (byte > 122 && byte <= 122 + shift_factor)
     end
+    result += byte.chr
+  end
   puts result
 end
-
-caesar_cipher("Hello dude", 4)
